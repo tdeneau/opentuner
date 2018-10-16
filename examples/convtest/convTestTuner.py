8bf6c8283e828632fd5755d7b942b514d8e7da3a
Tom Deneau <tom.deneau@amd.com>
2018-10-16 11:31:34 -0500

    convTestTuner options for using int param, showing diffs, etc.

--- examples/convtest/convTestTuner.py
+++ examples/convtest/convTestTuner.py
@@ -18,6 +18,12 @@ from opentuner import Result
 argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
 argparser.add_argument(
   '--num-params', type=int, default=300, help='number of parameters to use')
+argparser.add_argument(
+  '--show-diffs', default=False, action='store_true', help='show changes at each generation')
+argparser.add_argument(
+  '--use-enum-param', default=False, action='store_true', help='use enum instead of boolean param')
+argparser.add_argument(
+  '--use-int-param', default=False, action='store_true', help='use Integer instead of boolean param')
 
 class ConvTestTuner(opentuner.measurement.MeasurementInterface):
 
@@ -27,6 +33,7 @@ class ConvTestTuner(opentuner.measurement.MeasurementInterface):
     print args
     self.goals={}
     self.testnum = 0
+    self.prevcfg = None
     
   def manipulator(self):
     """
@@ -40,9 +47,19 @@ class ConvTestTuner(opentuner.measurement.MeasurementInterface):
     weightTotal = 0
     for n in range(self.args.num_params):
       pname = 'P%03d' % (n)
-      m.add_parameter(manipulator.BooleanParameter(pname))
-      # m.add_parameter(manipulator.EnumParameter(pname, ['on', 'off']))
-      self.goals[pname] = random.randint(0,1) == 0 
+      randbool = random.randint(0,1) == 0
+      if self.args.use_enum_param:
+        m.add_parameter(manipulator.EnumParameter(pname, ['on', 'off']))
+        self.goals[pname] = 'off' if randbool else 'on'
+      elif self.args.use_int_param:
+        m.add_parameter(manipulator.IntegerParameter(pname, 256, 257))
+        self.goals[pname] = 256 if randbool else 257
+      elif self.args.use_logint_param:
+        m.add_parameter(manipulator.LogIntegerParameter(pname, 256, 257))
+        self.goals[pname] = 256 if randbool else 257
+      else:
+        m.add_parameter(manipulator.BooleanParameter(pname))
+        self.goals[pname] = randbool 
       self.trace[pname] = {}
       weight = n*n + 1
       self.weight[pname] = weight
@@ -72,6 +89,14 @@ class ConvTestTuner(opentuner.measurement.MeasurementInterface):
       else:
         nonMatches += 1
         lastNonMatch = p
+    if self.args.show_diffs and self.prevcfg is not None:
+      print '%d:' % (self.testnum),
+      for p in sorted(cfg.keys()):
+        if cfg[p] != self.prevcfg[p]:
+          print p,
+      print
+
+       
         
     if score < self.best:
       self.best = score
@@ -89,7 +114,8 @@ class ConvTestTuner(opentuner.measurement.MeasurementInterface):
       if lastNonMatch is None:
         print 'reached perfect score, exiting'
         sys.exit(0)
-        
+
+    self.prevcfg = cfg
     return Result(time=score)
 
   
@@ -99,10 +125,11 @@ class ConvTestTuner(opentuner.measurement.MeasurementInterface):
       cfgDict = self.bestcfg
     else:
       cfgDict = finalCfg.data
-    print 'best score was %d' % self.best
+    print 'best score was %d, discrepancies follow:' % self.best
     for p in sorted(cfgDict.keys()):
       if cfgDict[p] != self.goals[p]:
-        print '(%s, weight %d)  ' % (p, self.weight[p]), 
+        print '(%s, weight %d)  ' % (p, self.weight[p]),
+    
 
   
 if __name__ == '__main__':
